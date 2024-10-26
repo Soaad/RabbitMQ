45affc6c776b36b1c097a38d4336d92f2725146f
Soaad Sayed <ssayed.c@NEC.GOV.SA>
2024-10-26 12:06:20 +0300

    topic exchange

---
+++ TopicExchange.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+Console.WriteLine("[*] waiting for message");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[X] recieved message {message}");
+
+};
+
+channel.BasicConsume(queue: "q.health1", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Press enter to exit");
+Console.ReadLine();
